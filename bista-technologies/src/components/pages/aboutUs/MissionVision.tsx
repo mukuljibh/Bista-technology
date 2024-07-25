9dfe4f0b1b428b4a2317486631d697af3bd3f76e
Nithin <nithin958595@gmail.com>
2024-07-25 19:58:53 +0530

    Mission and Vission page frontend

--- bista-technologies/src/components/pages/aboutUs/MissionVision.tsx
+++ bista-technologies/src/components/pages/aboutUs/MissionVision.tsx
@@ -1,7 +1,91 @@
-export default function MissionVision() {
-    return (
-        <div>
-            hello form mission and vision
+
+export default function MissionVision(){
+    return(<>
+    <div className="page-body mt-10">
+        <div className="img-container flex-col items-center pb-10">
+        <img 
+    src="https://www.bistatechnologies.com/static/1.jpg" 
+    className="mx-auto rounded-lg transition duration-300 ease-in-out transform hover:scale-105 hover:border-gray-500 border-2 border-transparent hover:border-opacity-50"
+/>
         </div>
-    )
+        <div className="info-container">
+            <div className="info-cards">
+                
+                <a href="#" className="flex m-2 flex-col items-center bg-white border border-gray-200  rounded-lg shadow md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                   <img className="object-cover w-full rounded-t-lg h-60 md:h-80 md:w-48 md:rounded-none md:rounded-s-lg" src="https://www.bistatechnologies.com/static/whoweare.jpg" alt=""/>
+                   <div className="flex flex-col justify-between p-4 leading-normal">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Who We Are</h5>
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Established in 2023, Bista Technologies Inc. is a 100% dynamic global staffing organization headquartered in Noida, India, with a mission to connect businesses with top-tier talent worldwide.</p>
+                   </div>
+                </a>
+                <a href="#" className="flex m-2 flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                  <div className="flex  flex-col justify-between p-4 leading-normal md:order-1">
+                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Who We Are</h5>
+                      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Bista Technologies Inc. specializes in providing comprehensive staffing solutions tailored to the unique needs of businesses across industries and continents. From IT to healthcare, engineering, and beyond, we leverage our expertise to deliver customized staffing solutions that drive success for our clients.</p>
+                  </div>
+                    <img className="object-cover w-full  h-60 md:h-80 md:w-48 md:rounded-none md:rounded-r-lg md:order-2" src="https://www.bistatechnologies.com/static/whatwedo.jpg" alt=""/>
+                </a>
+
+                <a  className="flex m-2 flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                   <img className="object-cover  w-full rounded-t-lg h-60 md:h-80 md:w-48 md:rounded-none md:rounded-s-lg" src="https://www.bistatechnologies.com/static/ourapproach.jpg" alt=""/>
+                   <div className="flex flex-col justify-between p-4 leading-normal">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Our Approaches:</h5>
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">We believe in the power of personalized service and industry expertise. Our team of dedicated professionals works closely with each client to understand their specific requirements and deliver customized staffing solutions that drive success. With a focus on excellence, integrity, and innovation, we strive to exceed our clients' expectations at every turn.</p>
+                   </div>
+                </a>
+            </div>
+  <div className="mission-vision-cards flex flex-col md:flex-row gap-4">
+    <a className="flex m-2 flex-col md:flex-row items-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 w-full md:w-1/2">
+        <div className="flex-shrink-0">
+            <img className="w-24 h-24 rounded-full md:ml-4" src="https://www.bistatechnologies.com/static/mission.png" alt="Mission"/>
+        </div>
+        <div className="flex flex-col justify-center p-4 leading-normal text-center md:text-left">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Mission</h5>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">"At Bista Technologies Inc., our mission is to be the premier global partner for workforce solutions, empowering businesses across borders with top-tier talent. Through our commitment to excellence, innovation, and cultural sensitivity, we aim to connect organizations with the right people, at the right time, to drive sustainable growth and success in a dynamic world."</p>
+        </div>
+    </a>
+    <a className="flex flex-col md:flex-row items-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 m-2 w-full md:w-1/2">
+        <div className="flex-shrink-0">
+            <img className="w-24 h-24 rounded-full md:ml-4" src="https://www.bistatechnologies.com/static/company-vision-icon-T.png" alt="Vision"/>
+        </div>
+        <div className="flex flex-col justify-center p-4 leading-normal text-center md:text-left">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Vision</h5>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">"Bista Technologies Inc. envisions a world where talent knows no boundaries and opportunities abound for individuals and organizations alike. As a leading global staffing organization, we strive to break down barriers, transcend borders, and foster meaningful connections that transcend geographical limitations. By leveraging our international expertise, diverse networks, and unwavering dedication to quality, we aspire to be the trusted partner of choice for businesses seeking to build winning teams on a global scale."</p>
+        </div>
+    </a>
+  </div>
+
+
+ </div>
+ <div className="p-4">
+    <div className="heading mb-4 mb-10 mt-10">
+    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-gray text-center">Why Choose Us:</h5>
+    </div>
+    <div className="flex flex-row flex-wrap justify-center gap-4">
+        <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-100  m-2 dark:hover:bg-red-100 dark:bg-red-400 w-full md:w-1/5">
+            <div className="flex flex-col justify-between p-4 leading-normal">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Global Reach</h5>
+                <p className="mb-3 font-normal text-gray-700 dark:text-black-400">With a presence in key markets around the world, we have the capacity to source talent internationally, ensuring our clients access the best candidates no matter where they are located.</p>
+            </div>
+        </a>
+
+        <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100  m-2 dark:hover:bg-blue-200 dark:bg-blue-700 w-full md:w-1/5">
+            <div className="flex flex-col justify-between p-4 leading-normal">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Industry Expertise</h5>
+                <p className="mb-3 font-normal text-gray-700 dark:text-black-400">Our team comprises seasoned professionals with deep industry knowledge and expertise, allowing us to provide insightful guidance and tailored solutions to our clients across various sectors.</p>
+            </div>
+        </a>
+
+        <a href="#" className="flex flex-col items-center bg-white   rounded-lg shadow dark:bg-gray-500  m-2 dark:hover:bg-gray-200 dark:bg-black-100 w-full md:w-1/5">
+            <div className="flex flex-col justify-between p-4 leading-normal">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Commitment to Quality</h5>
+                <p className="mb-3 font-normal text-black-700 dark:text-gray-600">We are committed to excellence in everything we do, from candidate sourcing to client service. Our dedication to quality ensures that our clients receive the highest standard of staffing solutions, every time.</p>
+            </div>
+        </a>
+    </div>
+</div>
+
+
+</div>
+    </>)
 }
