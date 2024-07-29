048b6b385794c13e9b9e684d43e7ce3ce7bcf006
Mukul <mukulbhardwaj966@gmail.com>
2024-07-29 19:21:52 +0530

    making ready again for production

--- bista-technologies/src/main.tsx
+++ bista-technologies/src/main.tsx
@@ -4,7 +4,6 @@ import './index.css'
 import { ScreenSizeProvider } from './contexts/ScreenSizeProvider.tsx'
 import { BrowserRouter } from "react-router-dom";
 import ScrollToTop from './ScrollToTop.tsx'
-import React from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ScreenSizeProvider>
