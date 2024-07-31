359c9dbac527b244764a10add1aec4a9601b631a
anurag-2420 <anuragpatel20june@gmail.com>
2024-07-31 23:53:10 +0530

    update

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,9 @@
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
+dotenv.config();
 
-const sequelize = new Sequelize('Bista_Tech', 'anurag', 'anurag123', {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER,process.env.MYSQL_PASSWORD, {
+    host: process.env.MYSQL_HOST,
     dialect: 'mysql'
   });
   
