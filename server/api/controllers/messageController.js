354cb819c4c0ca745c27138ee297f447f9d97402
anurag-2420 <anuragpatel20june@gmail.com>
2024-07-31 23:43:32 +0530

    Changes

--- server/api/controllers/messageController.js
+++ server/api/controllers/messageController.js
@@ -7,7 +7,9 @@ const leaveMessage = async (req, res) => {
      var decodedEmail = decodeURIComponent(req.body.email);
      const email=decodedEmail||req.body.email;
      let transporter = nodemailer.createTransport({
-         service: 'gmail',
+         host: 'smtp.office365.com',
+         port: 587,
+         secure: false,
          auth: {
              user: process.env.EMAIL,
              pass: process.env.PASSWORD
