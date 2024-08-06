504f3bfad5a8d95277daa72973c25d7ef755ba6d
anurag-2420 <anuragpatel20june@gmail.com>
2024-08-06 15:25:38 +0530

    Employer crud done with otp verification

--- server/db/db.js
+++ server/db/db.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
     process.env.MYSQL_PASSWORD, 
     {
        host: process.env.MYSQL_HOST,
-       port: process.env.MYSQL_PORT,
+      //  port: process.env.MYSQL_PORT,
        logging: false,
        dialect: 'mysql'
     }
