5a7f337a186dd04ab2d4fab452e4de481c8fd255
Mukul <mukulbhardwaj966@gmail.com>
2024-09-02 18:01:10 +0530

    created sample floating object

--- bista-technologies/src/components/pages/homepage/HomePage.tsx
+++ bista-technologies/src/components/pages/homepage/HomePage.tsx
@@ -2,12 +2,13 @@ import NewsLetter from "./newsletter/NewsLetter"
 import useObserver from "../../../shared/hooks/useObserver"
 import VisionStatementVideo from "./vision-statement-video/VisionStatementVideo"
 import MsmeBanner from "./msme-banner/MsmeBanner"
-import { hiringData } from "./config/home_page.config"
+import { hiringData } from "./config/homepage.config"
 import { motion } from "framer-motion"
 import HiringMetrics from './hiring-metrics/HiringMetrics'
 import CoreValuesBanner from "./core-values-banner/CoreValuesBanner"
 import OurServices from "./our-services/OurServices"
 import QuickServicesBar from "./quick-service-bar/QuickServicesBar"
+import CleintSay from "./cleint-say/CleintSay"
 export default function HomePage() {
     const { isCardVisible: HiringMetricsVisible } = useObserver("hiringMetrics", 0.5)
     return (
@@ -21,13 +22,14 @@ export default function HomePage() {
             <QuickServicesBar />
             <MsmeBanner />
             <OurServices />
-            <div className="hiringMetrics lg:py-20 px-10 my-10 lg:flex  lg:space-y-0 justify-center gap-10 space-y-10">
+            <div className="hiringMetrics  lg:py-20 px-10 my-10 lg:flex lg:space-y-0 justify-center gap-10 space-y-10">
                 {HiringMetricsVisible && hiringData.map((item, index) => {
                     return (
                         <HiringMetrics item={item} key={index} />
                     )
                 })}
             </div>
+            <CleintSay />
             <CoreValuesBanner />
             <NewsLetter />
         </motion.div >
--- bista-technologies/src/components/pages/homepage/vision-statement-video/VisionStatementVideo.tsx
+++ bista-technologies/src/components/pages/homepage/vision-statement-video/VisionStatementVideo.tsx
@@ -4,7 +4,7 @@ export default function VisionStatementVideo() {
     return (
         <div className="relative flex justify-center items-center  w-full min-h-screen ">
             <div className="lg:mt-28 mt-20 absolute text-center z-10 lg:font-normal font-semibold lg:space-y-5 space-y-2 " >
-                <div className="2xl:text-6xl md:text-3xl  md:text-3xl text-3xl font-semibold text-white transition-colors duration-300 cursor-pointer">
+                <div className="2xl:text-6xl md:text-3xl  text-3xl font-semibold text-white transition-colors duration-300 cursor-pointer">
                     Your Vision, Our Talent Solutions
                 </div>
 
