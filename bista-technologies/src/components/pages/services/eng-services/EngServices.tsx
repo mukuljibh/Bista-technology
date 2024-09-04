fba9cc2fa535269d7ae225060280691f71fb50e1
Mukul <mukulbhardwaj966@gmail.com>
2024-09-04 13:58:15 +0530

    optimized images on eng servicespage

--- bista-technologies/src/components/pages/services/eng-services/EngServices.tsx
+++ bista-technologies/src/components/pages/services/eng-services/EngServices.tsx
@@ -1,4 +1,4 @@
-import { engServicesConfig, engServicesCardsConfig } from "./config/engServicesConfig"
+import { mainCardsConfig, cardsConfig } from "./config/engServicesConfig"
 import useObserver from "../../../../shared/hooks/useObserver"
 import Card from "../../../../ui/merged_components/service-cards/Card"
 import MainCard from "../../../../ui/merged_components/service-cards/mainCard/MainCard"
@@ -16,19 +16,26 @@ export default function EngServices() {
             animate={{ translateX: '0vh', }}
             exit={{ transition: { duration: 0.2 } }}
             transition={{ duration: 0.6 }}
-            className="space-y-10 bg-slate-50   ">
-            <Helmet><title>Engineering Staffing Solutions</title></Helmet>
+            className=" bg-slate-50   ">
+
+            <Helmet>
+                <meta name="description" content="Explore our Engineering Staffing Solutions to scale up your team with skilled professionals. We offer customized staffing services to meet your engineering needs. Enhance your workforce today with Bista Technologies." />
+                <meta name="keywords" content="Engineering Staffing Solutions, Engineering Services, Skilled Professionals, Staffing Services, Workforce Scaling, Bista Technologies, Engineering Team, Staffing Solutions, IT Staffing, Engineering Recruitment" />
+                <title>Engineering Staffing Solutions</title>
+            </Helmet>
+
             <div className="space-y-20 mb-16">
-                <img className="w-full " src="https://www.bistatechnologies.com/static/9.jpg" />
+                <img className="w-full" src="/services/eng-services/1.webp" alt="eng-services banner" width="1920" height="600" />
                 <h1 className="text-4xl  font-semibold text-center text-gray-700">Welcome to Engineering Staffing Solutions</h1>
             </div>
-            {engServicesConfig.map(({ heading1, heading2, imageSrc, description, cardCss, headingCss }, index) => {
+            {mainCardsConfig.map(({ heading1, heading2, image, description, cardCss, headingCss }, index) => {
                 return (
                     <MainCard
                         key={index}
                         heading1={heading1}
                         heading2={heading2}
-                        imageSrc={imageSrc}
+                        imageSrc={image.imageSrc}
+                        alt={image.alt}
                         description={description}
                         cardCss={cardCss}
                         headingCss={headingCss}
@@ -38,14 +45,14 @@ export default function EngServices() {
             })}
             <div className="bg-purple-50 py-20 space-y-10">
                 <h1 className="text-3xl text-gray-700 text-center font-semibold">Scale Up Your Team The Way You Want</h1>
-                <div className="ITCards  flex flex-wrap justify-evenly gap-5 lg:px-15 px-10 lg:space-y-0 space-y-10">
-                    {isCardVisible && engServicesCardsConfig.map((item, index) => {
+                <div className="ITCards  flex flex-wrap justify-evenly gap-5 lg:px-15 px-5 lg:space-y-0 space-y-10">
+                    {isCardVisible && cardsConfig.map((item, index) => {
                         return <Card
                             key={index}
                             heading={item.heading}
                             description={item.description}
-                            imgSrc={item.imageSrc}
-                            alt="engineering services logo"
+                            imgSrc={item.image.imageSrc}
+                            alt={item.image.alt}
                             css={item.css}
                             animationTime={item.animationTime} />
                     })}
--- bista-technologies/src/ui/merged_components/service-cards/mainCard/MainCard.tsx
+++ bista-technologies/src/ui/merged_components/service-cards/mainCard/MainCard.tsx
@@ -3,19 +3,25 @@ type mainCardProps = {
     heading1: string
     heading2: string
     imageSrc: string
+    alt?: string
     description: string
     cardCss: string
     headingCss: string
     observingClass: string
 }
 
-export default function MainCard({ heading1, heading2, imageSrc, description, cardCss, headingCss, observingClass }: mainCardProps) {
+export default function MainCard({ heading1, heading2, imageSrc, alt, description, cardCss, headingCss, observingClass }: mainCardProps) {
 
     return (
         <div className={`${cardCss} py-4 min-h-screen ${observingClass} opacity-0 duration-1000 delay-100 items-center justify-evenly  px-5 space-y-4 lg:h-96 transition-all`
         }>
             <div className={`lg:w-2/5 p-14 h-full px-3`}>
-                <img className="w-full lg:h-full object-cover rounded-lg " src={imageSrc} />
+                <img className="w-full lg:h-full object-cover rounded-lg "
+                    src={imageSrc}
+                    alt={alt}
+                    width="800"
+                    height="600"
+                />
             </div>
             <div className={`max-w-2xl space-y-3`}>
                 <h1 className={`inline px-4 py-1 rounded-2xl font-normal ${headingCss}`}>{heading1}</h1>
