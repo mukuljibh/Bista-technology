f0a5c8a54b2c81c8796a3615f3db384acc9be5ff
Nithin <nithin958595@gmail.com>
2024-09-05 23:50:53 +0530

    created config folder for leadgeration page

---
+++ bista-technologies/src/components/pages/solutions/config/LeadGeneration.ts
@@ -0,0 +1,29 @@
+
+const leadGenData = [
+    {
+      title: 'B2B Lead Generation:',
+      description:
+        'We provide targeted B2B lead generation services to help businesses identify and connect with potential clients and partners. Our strategic approach ensures high-quality leads that align with your business objectives.',
+      imgSrc: 'https://www.bistatechnologies.com/static/leadgen(2).jpg',
+    },
+    {
+      title: 'Market Research:',
+      description:
+        'Our team conducts in-depth market research to identify emerging trends, potential markets, and key decision-makers. This data-driven approach helps us generate leads that are more likely to convert into valuable business opportunities.',
+      imgSrc: 'https://www.bistatechnologies.com/static/leadgen(3).jpg',
+    },
+    {
+      title: 'Digital Marketing Campaigns:',
+      description:
+        'Leveraging the latest digital marketing techniques, we create and execute campaigns designed to attract and engage potential leads. From SEO to social media marketing, our strategies drive traffic and generate high-quality leads.',
+      imgSrc: 'https://www.bistatechnologies.com/static/leadgen(4).jpg',
+    },
+    {
+      title: 'Database Management:',
+      description:
+        'We manage and maintain comprehensive databases of potential leads, ensuring that your sales team has access to up-to-date information and valuable insights. Our database management services help streamline the lead generation process and improve conversion rates.',
+      imgSrc: 'https://www.bistatechnologies.com/static/leadgen(5).jpg',
+    },
+  ];
+
+  export {leadGenData};
